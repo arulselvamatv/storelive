53e8f26193fcb6dc6225ac2be8b0a190c898e914
Anand Techverce <65645747+arulselvamatv@users.noreply.github.com>
2023-05-20 14:00:09 +0530

    Update quotion.php

--- admin/quotion.php
+++ admin/quotion.php
@@ -3,6 +3,9 @@
 require_once("header.php");
 
 require_once("database/connect.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $db = new Database;
 
